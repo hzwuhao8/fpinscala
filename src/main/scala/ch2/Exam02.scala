d325191a22d3c1ca5898d76988eea90b6010ed76
wuhao <hzwuhao8@gmail.com>
2016-07-27 18:15:54 +0800

    实现了 参数是 Array 的版本

--- src/main/scala/ch2/Exam02.scala
+++ src/main/scala/ch2/Exam02.scala
@@ -10,12 +10,22 @@ object Exam02 {
     }
   }
 
+  def isSorted[A](as: Array[A])(ordered: (A, A) => Boolean): Boolean = {
+    as match {
+      case Array()       => true
+      case Array(x)      => true
+      case Array(x1, x2) => ordered(x1, x2)
+      case _             => isSorted(as.drop(1))(ordered)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     def o(x: Int, y: Int) = x < y
     val r1 = isSorted((1 to 10).toList)(o)
     println(r1)
-    val r2 = isSorted((10.to(1,-1)).toList)(o)
+    val r2 = isSorted((10.to(1, -1)).toList)(o)
     println(r2)
     println(isSorted(Nil)(o))
+    println(isSorted(Array(1, 2, 3, 10, 9))(o))
   }
 }
