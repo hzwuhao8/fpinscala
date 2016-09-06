ce6d900e4b137a8793371f9075574d59493ff4c2
wuhao <wuhao@bokee.net>
2016-09-06 19:19:37 +0800

    choose unit

--- src/main/scala/ch8/Check.scala
+++ src/main/scala/ch8/Check.scala
@@ -4,37 +4,52 @@ import ch6.State
 import ch6.RNG
 import ch6.SimpleRNG
 
-trait MyGen[A] {
+
+
+trait Prop {
 
 }
+case class MyGen[A](sample: State[RNG, A]) {
 
-trait Prop{
   
-}
-case class MyGen[A](sample: State[RNG,A]){
+  def choose(start: Int, stop: Int): MyGen[Int] = {
+    def f(i: Int): Int = {
+      val d = stop - start
+      if (d == 0) {
+        start
+      } else {
+        start + i % d
+      }
+    }
+    val run = (rng: RNG) => {
+      val (i, nextRng) = rng.nextInt
+      (f(i), nextRng)
+    }
+
+    MyGen(State(run))
+  }
   
-  val rng: RNG = SimpleRNG(0L)
-  def choose(s: Int, e: Int): MyGen[Int] = {
-    val f = sample.run
-    val(a,nextRng) = f(rng)
-    val s = State( sample.run )
-    MyGen(s)
+  def unit[A](a: => A): MyGen[A] ={
+    val run = (rng: RNG) => {
+      val (i, nextRng) = rng.nextInt
+      (a , nextRng)
+    }
+     MyGen(State(run))
   }
 }
 
-object MyGen {
-  
+object MyGenX {
+
   def choose[A](a: A, b: A): MyGen[A] = {
-    val f =  { i: Int => a}
-    
+    val f = { i: Int => a }
+
   }
-  
-  
+
   def listOfN[A](n: Int, a: MyGen[A]): MyGen[List[A]] = {
 
   }
-  
-  def forAll[A](a: MyGen[A])(f: A=> Boolean): Prop ={
-    
+
+  def forAll[A](a: MyGen[A])(f: A => Boolean): Prop = {
+
   }
 }
