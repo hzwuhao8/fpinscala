2ddc89645aaf42d0f2ecf893914e698284495a7e
wuhao <wuhao@bokee.net>
2016-09-07 20:53:22 +0800

    脚手架  基本 搭好了
     six primitives: string, regex, slice, succeed, or, and flatMap

--- src/main/scala/ch9/Parse.scala
+++ src/main/scala/ch9/Parse.scala
@@ -2,6 +2,7 @@ package ch9
 import ch8.MyGen
 import ch8.MyGenX
 import ch8.Prop
+import scala.util.matching.Regex
 
 class ParseError {
 
@@ -13,8 +14,9 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+  implicit def regex(r: Regex): Parser[String]
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
 
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = {
     if (n > 0) {
@@ -29,13 +31,24 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   def many[A](p: Parser[A]): Parser[List[A]] = {
     map2(p, many(p))(_ :: _) or succeed(List())
   }
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B] = {
+    flatMap(a) { x => succeed(f(x)) }
+  }
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
   def succeed[A](a: A): Parser[A] = string("") map (_ => a)
 
   def slice[A](p: Parser[A]): Parser[String]
-  def many1[A](p: Parser[A]): Parser[List[A]]
-  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)]
+
+  def many1[A](p: Parser[A]): Parser[List[A]] = {
+    map2(p, many1(p))(_ :: _) or (p.map { x => List(x) })
+  }
+
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] = {
+    def f(a: A) = { p2.map(b => (a, b)) }
+    flatMap(p)(f)
+  }
   def **[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] = product(p, p2)
 
   def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = {
@@ -44,10 +57,11 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   }
 
   case class ParserOps[A](p: Parser[A]) {
-    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def many: Parser[List[A]] = self.many(p)
     def map[B](f: A => B) = self.map(p)(f)
+    def flatMap[B](f: A => Parser[B]) = self.flatMap(p)(f)
     val numA: Parser[Int] = char('a').many.map { _.size }
 
   }
