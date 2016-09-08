336d5aa8bc44c6d0d707cdd105258340ca9e42f1
wuhao <wuhao@bokee.net>
2016-09-08 19:55:20 +0800

    编写 number2  支持 " "

---
+++ src/main/scala/ch9/ExamMath.scala
@@ -0,0 +1,51 @@
+package ch9
+
+import fastparse.all._
+
+object ExamMath {
+
+  def eval(tree: (Int, Seq[(String, Int)])) = {
+    val (base, ops) = tree
+    ops.foldLeft(base) {
+      case (left, (op, right)) => op match {
+        case "+" => left + right case "-" => left - right
+        case "*" => left * right case "/" => left / right
+      }
+    }
+  }
+
+  def main(args: Array[String]) {
+    val number1: P[Int] = P(CharIn('0' to '9').rep(1).!.map(_.toInt))
+
+    val number2: P[Int] = P(" ".rep ~ CharIn('0' to '9').rep(1) ~ " ".rep).!.map(s => s.trim().toInt)
+
+    val number: P[Int] = number2
+    lazy val parens: P[Int] = P("(" ~/ addSub ~ ")")
+    lazy val factor: P[Int] = P(number | parens)
+
+    lazy val divMul: P[Int] = P(factor ~ (CharIn("*/").! ~/ factor).rep).map(eval)
+    lazy val addSub: P[Int] = P(divMul ~ (CharIn("+-").! ~/ divMul).rep).map(eval)
+    lazy val expr: P[Int] = P(addSub ~ End)
+
+    def check(str: String, num: Int) = {
+      val Parsed.Success(value, _) = expr.parse(str)
+      assert(value == num)
+    }
+
+    {
+
+      println(number2.parse("1 "))
+      println(number2.parse(" 12 "))
+      println(number2.parse(" 123"))
+    }
+
+    check("1+1", 2)
+    check("1+1*2", 3)
+    check("(1+1*2)+(3*4*5)", 63)
+    check("15/3", 5)
+    check("63/3", 21)
+    check("(1+1*2)+(3*4*5)/20", 6)
+    check("((1+1*2)+(3*4*5))/3", 21)
+check("((1 +1* 2)+(3*4 *5))/3", 21)
+  }
+}
