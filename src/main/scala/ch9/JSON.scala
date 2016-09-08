2ca4bda97cfe612cf083db73e92a7963c6d3c119
wuhao <hzwuhao8@gmail.com>
2016-09-08 11:21:58 +0800

    Reference 这个是 分析器的 一个 参考实现 还是很复杂的

--- src/main/scala/ch9/JSON.scala
+++ src/main/scala/ch9/JSON.scala
@@ -1,5 +1,8 @@
 package ch9
 
+import language.higherKinds
+import language.implicitConversions
+
 trait JSON
 
 object JSON {
@@ -9,14 +12,70 @@ object JSON {
   case class JBool(get: Boolean) extends JSON
   case class JArray(get: IndexedSeq[JSON]) extends JSON
   case class JObject(get: Map[String, JSON]) extends JSON
-}
 
-object JSONP {
   def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] = {
-    import P._
-    val spaces = char(' ').many.slice
-    val nullP = string("null")
-    
-    
+    import P.{ string => _, _ }
+    implicit def tok(s: String) = token(P.string(s))
+
+    def array = surround("[", "]")(
+      value sep "," map (vs => JArray(vs.toIndexedSeq))) scope "array"
+    def obj = surround("{", "}")(
+      keyval sep "," map (kvs => JObject(kvs.toMap))) scope "object"
+    def keyval = escapedQuoted ** (":" *> value)
+    def lit = scope("literal") {
+      "null".as(JNull) |
+        double.map(JNumber(_)) |
+        escapedQuoted.map(JString(_)) |
+        "true".as(JBool(true)) |
+        "false".as(JBool(false))
+    }
+    def value: Parser[JSON] = lit | obj | array
+    root(whitespace *> (obj | array))
+
   }
 }
+
+/**
+ * JSON parsing example.
+ */
+object JSONExample extends App {
+  val jsonTxt = """
+{
+  "Company name" : "Microsoft Corporation",
+  "Ticker"  : "MSFT",
+  "Active"  : true,
+  "Price"   : 30.66,
+  "Shares outstanding" : 8.38e9,
+  "Related companies" : [ "HPQ", "IBM", "YHOO", "DELL", "GOOG" ]
+}
+"""
+
+  val malformedJson1 = """
+{
+  "Company name" ; "Microsoft Corporation"
+}
+"""
+
+  val malformedJson2 = """
+[
+  [ "HPQ", "IBM",
+  "YHOO", "DELL" ++
+  "GOOG"
+  ]
+]
+"""
+  val P = Reference
+  import ReferenceTypes.Parser
+
+  def printResult[E](e: Either[E, JSON]) =
+    e.fold(println, println)
+
+  val json: Parser[JSON] = JSON.jsonParser(P)
+  printResult { P.run(json)(jsonTxt) }
+  println("--")
+  printResult { P.run(json)(malformedJson1) }
+  println("--")
+  printResult { P.run(json)(malformedJson2) }
+}
+
+  
--- src/main/scala/ch9/Parse.scala
+++ src/main/scala/ch9/Parse.scala
@@ -3,10 +3,9 @@ import ch8.MyGen
 import ch8.MyGenX
 import ch8.Prop
 import scala.util.matching.Regex
+import java.util.regex.Pattern
 
-class ParseError {
-
-}
+ 
 
 trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
@@ -37,7 +36,7 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
-  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+  def succeed[A](a: A): Parser[A]  
 
   def slice[A](p: Parser[A]): Parser[String]
 
@@ -56,6 +55,86 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     p3.map { x => f(x._1, x._2) }
   }
 
+  def label[A](msg: String)(p: Parser[A]): Parser[A]
+
+  def scope[A](msg: String)(p: Parser[A]): Parser[A]
+
+  def attempt[A](p: Parser[A]): Parser[A]
+
+  /**
+   * Sequences two parsers, ignoring the result of the first.
+   * We wrap the ignored half in slice, since we don't care about its result.
+   */
+  def skipL[B](p: Parser[Any], p2: => Parser[B]): Parser[B] =
+    map2(slice(p), p2)((_, b) => b)
+
+  /**
+   * Sequences two parsers, ignoring the result of the second.
+   * We wrap the ignored half in slice, since we don't care about its result.
+   */
+  def skipR[A](p: Parser[A], p2: => Parser[Any]): Parser[A] =
+    map2(p, slice(p2))((a, b) => a)
+
+  def opt[A](p: Parser[A]): Parser[Option[A]] =
+    p.map(Some(_)) or succeed(None)
+
+  /** Parser which consumes zero or more whitespace characters. */
+  def whitespace: Parser[String] = "\\s*".r
+
+  /** Parser which consumes 1 or more digits. */
+  def digits: Parser[String] = "\\d+".r
+
+  /** Parser which consumes reluctantly until it encounters the given string. */
+  def thru(s: String): Parser[String] = (".*?" + Pattern.quote(s)).r
+
+  /** Unescaped string literals, like "foo" or "bar". */
+  def quoted: Parser[String] = string("\"") *> thru("\"").map(_.dropRight(1))
+
+  /** Unescaped or escaped string literals, like "An \n important \"Quotation\"" or "bar". */
+  def escapedQuoted: Parser[String] =
+    // rather annoying to write, left as an exercise
+    // we'll just use quoted (unescaped literals) for now
+    token(quoted label "string literal")
+
+  /**
+   * C/Java style floating point literals, e.g .1, -1.0, 1e9, 1E-23, etc.
+   * Result is left as a string to keep full precision
+   */
+  def doubleString: Parser[String] =
+    token("[-+]?([0-9]*\\.)?[0-9]+([eE][-+]?[0-9]+)?".r)
+
+  /** Floating point literals, converted to a `Double`. */
+  def double: Parser[Double] =
+    doubleString map (_.toDouble) label "double literal"
+
+  /** Attempts `p` and strips trailing whitespace, usually used for the tokens of a grammar. */
+  def token[A](p: Parser[A]): Parser[A] =
+    attempt(p) <* whitespace
+
+  /** Zero or more repetitions of `p`, separated by `p2`, whose results are ignored. */
+  def sep[A](p: Parser[A], p2: Parser[Any]): Parser[List[A]] = // use `Parser[Any]` since don't care about result type of separator
+    sep1(p, p2) or succeed(List())
+
+  /** One or more repetitions of `p`, separated by `p2`, whose results are ignored. */
+  def sep1[A](p: Parser[A], p2: Parser[Any]): Parser[List[A]] =
+    map2(p, many(p2 *> p))(_ :: _)
+
+  /** Parses a sequence of left-associative binary operators with the same precedence. */
+  def opL[A](p: Parser[A])(op: Parser[(A, A) => A]): Parser[A] =
+    map2(p, many(op ** p))((h, t) => t.foldLeft(h)((a, b) => b._1(a, b._2)))
+
+  /** Wraps `p` in start/stop delimiters. */
+  def surround[A](start: Parser[Any], stop: Parser[Any])(p: => Parser[A]) =
+    start *> p <* stop
+
+  /** A parser that succeeds when given empty input. */
+  def eof: Parser[String] =
+    regex("\\z".r).label("unexpected trailing characters")
+
+  /** The root of the grammar, expects no further input following `p`. */
+  def root[A](p: Parser[A]): Parser[A] =
+    p <* eof
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
@@ -64,6 +143,22 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def flatMap[B](f: A => Parser[B]) = self.flatMap(p)(f)
     val numA: Parser[Int] = char('a').many.map { _.size }
     def slice = self.slice(p)
+    def **[B](p2: => Parser[B]): Parser[(A, B)] =
+      self.product(p, p2)
+
+    def label(msg: String): Parser[A] = self.label(msg)(p)
+
+    def scope(msg: String): Parser[A] = self.scope(msg)(p)
+
+    def *>[B](p2: => Parser[B]) = self.skipL(p, p2)
+    def <*(p2: => Parser[Any]) = self.skipR(p, p2)
+
+    def token = self.token(p)
+    def sep(separator: Parser[Any]) = self.sep(p, separator)
+    def sep1(separator: Parser[Any]) = self.sep1(p, separator)
+    def as[B](b: B): Parser[B] = self.map(self.slice(p))(_ => b)
+    def opL(op: Parser[(A, A) => A]): Parser[A] = self.opL(p)(op)
+
   }
 
   object Laws {
@@ -71,5 +166,77 @@ trait Parsers[ParseError, Parser[+_]] { self =>
       MyGenX.forAll(in)(s => run(p1)(s) == run(p2)(s))
     def mapLaw[A](p: Parser[A])(in: MyGen[String]): Prop = equal(p, p.map(a => a))(in)
   }
+
+}
+
+case class Location(input: String, offset: Int = 0) {
+
+  lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
+  lazy val col = input.slice(0,offset+1).lastIndexOf('\n') match {
+    case -1 => offset + 1
+    case lineStart => offset - lineStart
+  }
+
+  def toError(msg: String): ParseError =
+    ParseError(List((this, msg)))
+
+  def advanceBy(n: Int) = copy(offset = offset+n)
+
+  /* Returns the line corresponding to this location */
+  def currentLine: String =
+    if (input.length > 1) input.lines.drop(line-1).next
+    else ""
+
+  def columnCaret = (" " * (col-1)) + "^"
+}
+case class ParseError(stack: List[(Location,String)] = List()) {
+  def push(loc: Location, msg: String): ParseError =
+    copy(stack = (loc,msg) :: stack)
+
+  def label[A](s: String): ParseError =
+    ParseError(latestLoc.map((_,s)).toList)
+
+  def latest: Option[(Location,String)] =
+    stack.lastOption
+
+  def latestLoc: Option[Location] =
+    latest map (_._1)
+  
+  /**
+  Display collapsed error stack - any adjacent stack elements with the
+  same location are combined on one line. For the bottommost error, we
+  display the full line, with a caret pointing to the column of the error.
+  Example:
+
+  1.1 file 'companies.json'; array
+  5.1 object
+  5.2 key-value
+  5.10 ':'
+
+  { "MSFT" ; 24,
+  */
+  override def toString =
+    if (stack.isEmpty) "no error message"
+    else {
+      val collapsed = collapseStack(stack)
+      val context =
+        collapsed.lastOption.map("\n\n" + _._1.currentLine).getOrElse("") +
+        collapsed.lastOption.map("\n" + _._1.columnCaret).getOrElse("")
+      collapsed.map { case (loc,msg) => loc.line.toString + "." + loc.col + " " + msg }.mkString("\n") +
+      context
+    }
+
+  /* Builds a collapsed version of the given error stack -
+   * messages at the same location have their messages merged,
+   * separated by semicolons */
+  def collapseStack(s: List[(Location,String)]): List[(Location,String)] =
+    s.groupBy(_._1).
+      mapValues(_.map(_._2).mkString("; ")).
+      toList.sortBy(_._1.offset)
+
+  def formatLoc(l: Location): String = l.line + "." + l.col
+}
+
+object Parsers {
 
 }
