6ab545c1547a27560c68fe74169504bb83b032e6
wuhao <wuhao@bokee.net>
2016-09-10 20:58:04 +0800

    Monad 基础

--- build.sbt
+++ build.sbt
@@ -5,6 +5,6 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.2" % "test"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.2" 
 
 libraryDependencies += "com.lihaoyi" %% "fastparse" % "0.3.7"
--- src/main/scala/ch11/Monad.scala
+++ src/main/scala/ch11/Monad.scala
@@ -21,8 +21,67 @@ trait Monad[F[_]] {
     la.foldRight(unit(List[B]())) { (a, b) => map2(f(a), b)(_ :: _) }
   }
 
+  def replicateM[A](n: Int, ma: F[A]): F[List[A]] = {
+    sequence(List.fill(n)(ma))
+  }
+  // Recursive version:
+  def _replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    if (n <= 0) unit(List[A]()) else map2(ma, _replicateM(n - 1, ma))(_ :: _)
+
+  def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] = {
+    val res1 = traverse(ms) { a =>
+      val r1 = map(f(a)) { b => (a, b) }
+      r1
+    }
+    val res2 = map(res1)(_.filter(_._2).map(_._1))
+
+    res2
+  }
+
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => flatMap(f(a))(g)
+
+  def flatMap2[A, B](ma: F[A])(f: A => F[B]): F[B] = {
+    val ff = (a: Int) => ma
+    val gg = f
+    val cc = compose(ff, gg)
+    cc(1)
+  }
+
+  def join[A](mma: F[F[A]]): F[A] = {
+    flatMap(mma)((fa) => fa)
+  }
+
+  def flatMap3[A, B](ma: F[A])(f: A => F[B]): F[B] = {
+    val res = map(ma)(f)
+    join(res)
+  }
+
+  def compose3[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] = {
+    (a: A) =>
+      join(map(f(a))(g))
+
+  }
+
 }
 
+case class Id[A](value: A) {
+  def unit[A](a: => A): Id[A] = Id(a)
+
+  def map[B](f: A => B): Id[B] = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+}
+
+object IdMonad extends Monad[Id] {
+  def unit[A](a: => A): Id[A] = {
+    Id(a)
+  }
+
+  def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = {
+    f(ma.value)
+  }
+
+}
 object Monad {
   import ch8.MyGen
   val genMonad = new Monad[MyGen] {
